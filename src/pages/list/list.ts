e56b7749719fa40217c7d3d6b0d2bb9c13706f70
shyamal63 <gayen.shyamal@gmail.com>
2018-05-21 16:50:39 +0530

    complete app

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import * as firebase from 'firebase'
 import { UserDetailsPage } from '../user-details/user-details';
+import { LoadingController } from 'ionic-angular/components/loading/loading-controller';
+import { LoginPage } from '../login/login';
 
 @Component({
   selector: 'page-list',
@@ -12,32 +14,50 @@ export class ListPage {
 public myArray:any=[];
 public myData:any;
 public items:any;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+constructor(public navCtrl: NavController, public navParams: NavParams,public loadingCtrl: LoadingController) {
  
-   
-  }
+  let loading = this.loadingCtrl.create({
+    content: 'Please wait...'
 
-  ionViewDidLoad() {
+  });
+
+  loading.present();
+  setTimeout(() => {
+    loading.dismiss();
+  },1000), 
+
+  this.loadUser();   
+}
+
+  loadUser(){
     const personRef: firebase.database.Reference = firebase.database().ref(`/userProfile/`);
-    var currentUser = firebase.auth().currentUser;
-    
-  
+    let currentUser = firebase.auth().currentUser.uid;
+    console.log(currentUser);
     personRef.on('value', personSnapshot => {
       this.items = [];
-      personSnapshot.forEach( itemSnap => {
-        console.log(itemSnap);
-        this.items.push(itemSnap.val());
+      console.log(personSnapshot.val());
+      let newItem=personSnapshot.val();
+     for(let k in newItem){
+      newItem[k].userId = k;
+      if(newItem[k].userId != currentUser){
+        this.items.push(newItem[k]);          
+       }
         console.log(this.items);
-        return false;
-      })      
+      }  
+        
     });
-    
+  }
+
+  ionViewDidLoad() { 
     }
+  itemTapped($event, newItem){
+    console.log(newItem);
+    this.navCtrl.push(UserDetailsPage, {userData: newItem});
+  }
 
-
-  itemTapped($event, item){
-    console.log(item);
-    this.navCtrl.push(UserDetailsPage, {userData: item});
+  logOut(){
+    this.navCtrl.setRoot(LoginPage);
+    firebase.auth().signOut
   }
  
 }
--- src/pages/user-details/user-details.ts
+++ src/pages/user-details/user-details.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
 import * as firebase from 'firebase'
+import { LoadingController } from 'ionic-angular/components/loading/loading-controller';
+import { LoginPage } from '../login/login';
 
 @IonicPage()
 @Component({
@@ -9,33 +10,77 @@ import * as firebase from 'firebase'
   templateUrl: 'user-details.html',
 })
 export class UserDetailsPage {
-
   public userMsg: string;
   public userDetails: any;
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-
+  public messegeArray:any
+  public userNm:any;
+  public userMessegeId:any;
+  public messegeUserName:any;
+  public currentUser:any
+  constructor(public navCtrl: NavController, public navParams: NavParams,public loadingCtrl: LoadingController) {
+    
+    let loading = this.loadingCtrl.create({
+      content: 'Please wait...'
+    });
+  
+    loading.present();
+    setTimeout(() => {
+      loading.dismiss();
+    },700), 
+  
+    this.currentUser = firebase.auth().currentUser.uid;
+    console.log(this.currentUser)
     this.userDetails = this.navParams.get('userData');
     console.log(this.userDetails);
+    const personRef: firebase.database.Reference = firebase.database().ref(`/userProfile/`+this.userDetails.userId);
+    personRef.on('value', personSnapshot => {
+    console.log(personSnapshot.val());
+    
+    let messeges=personSnapshot.val().messege
+    
+    this.messegeArray=[];
+    for(let data in messeges){
+      if(this.currentUser==messeges[data].msguserId){
+      this.messegeArray.push(messeges[data]);
+    }
+    console.log(this.messegeArray);
+
+  }
+    });
+
+    
+  
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad UserDetailsPage');
-  }
 
+  }
   sendMsg(){
-    var currentUser = firebase.auth().currentUser;
-    console.log(currentUser);
-   
-    firebase.database().ref('/userProfile/' +currentUser.uid + "/messege/").push({
-      userMessege:this.userMsg
     
+    const personRef: firebase.database.Reference = firebase.database().ref(`/userProfile/`+this.currentUser);
+   
+    personRef.on('value', personSnapshot => {
+    console.log(personSnapshot.val()); 
+    this.messegeUserName=personSnapshot.val().username;
+    console.log(this.messegeUserName)
+    });
+    
+  
+    firebase.database().ref('/userProfile/' + this.userDetails.userId+ "/messege/").push({
+      userMessege:this.userMsg,
+      msguserId:this.currentUser,
+      messegeUser:this.messegeUserName
+      
     })
     console.log(this.userMsg);
     this.userMsg='';
     
-
     
+  }
+  logOut(){
+    this.navCtrl.setRoot(LoginPage);
+    firebase.auth().signOut
   }
 
 }
